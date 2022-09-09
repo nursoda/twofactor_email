33bc1b74e21687e67f0292b5736c5fd2c3042449
Olav Seyfarth <olav@seyfarth.de>
2022-09-09 10:24:41 +0200

    Add Brazilian Portuguese translation (José A. Marteline Cavalcante)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,18 @@
 # Changelog
 Notable changes in [changelog format](https://keepachangelog.com/en/1.0.0/), project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
 
+## 2.7.0 (2022-09-1x)
+
+### Added
+
+* Portuguese translation (@NN)
+
+### Changed
+
+* add light and gray icons
+
 ## 2.6.0 (2022-09-04)
+
 ### Added
 - Support for Nextcloud 25 (tested against beta 4)
 ### Changed
---
+++ l10n/pt_BR.js
@@ -0,0 +1,35 @@
+OC.L10N.register(
+"twofactor_email",
+{
+	// lib/Provider/Email.php
+	"Email" : "E-Mail",
+	"Send a code to your email address" : "Envie um código para seu endereço de e-mail",
+
+	// lib/Service/Email.php
+	"Login attempt for %s" : "Tentativa de login para %s",
+	"Your two-factor authentication code is: %s" : "Seu código de autenticação de dois fatores é: %s",
+	"If you tried to login, please enter that code on %s. If you did not, somebody else did and knows your your email address or username – and your password!" : "Se você tentou fazer o login, por favor, digite este código em %s. Se você não tentou, alguém tentou e sabe o seu login e a sua senha!",
+
+	// src/components/GatewaySettings.vue
+	"You need to set an email address in 'Personal info' first." : "Você precisa definir primeiro o seu e-mail em 'Informações pessoais'",
+
+	"Could not send a verification code via email. An Admin must set this up first." : "Não foi possível enviar um código de verificação por e-mail. As configurações iniciais ainda precisam ser realizadas pelo administrador.",
+	"Enable Two-Factor Authentication via Email" : "Habilitar autenticação de dois fatores via e-mail",
+
+	"The entered code does not match that sent to {emailAddress}." : "O código inserido não corresponde ao enviado para {emailAddress}.",
+	"A code has been sent to {emailAddress}." : "Um código foi enviado para {emailAddress}.",
+	"Verify code" : "Verificar o código",
+	"Cancel activation" : "Cancelar ativação",
+
+	"Two-Factor Authentication via Email is enabled. Codes are sent to {emailAddress}." : "A autenticação de dois fatores via e-mail está habilitada. Os códigos serão enviados para {emailAddress}.",
+	"Disable Two-Factor Authentication via Email" : "Desabilitar a autenticação de dois fatores via e-mail",
+
+	// templates/error.php 
+	"Error while sending the email. Please try again later or ask your administrator." : "Erro ao enviar o e-mail. Por favor, tente novamente mais tarde ou entre em contato com o seu administrador",
+
+	// templates/challenge.php
+	"A code has been sent to your email address." : "Um código foi enviado para o seu e-mail.",
+	"Authentication code" : "Código de autenticação",
+	"Submit" : "Submeter",
+},
+"");
